c05f6809f9463827e9a0b9ba29b88555785b6e03
aidlvsk <58606412+aidlvsk@users.noreply.github.com>
2021-06-27 15:06:19 +0300

    Отрисуй меня полностью, часть 1, правки 2 и задание module7-task2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import {createAdvt} from './creators.js';
 import './generate.js';
+import {noActiveForm} from './formActivation.js';
 
 function allAdvt(count) {
   return new Array(count).fill(null).map(()=>createAdvt());
 }
 
 allAdvt(10);
+noActiveForm();
